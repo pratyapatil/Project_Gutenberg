1421be2425d31c912a3a8e262a3bca2081545d8f
pratyapatil <117253773+pratyapatil@users.noreply.github.com>
2024-03-19 18:12:44 +0530

    Update admin.py

--- Project_Gutenberg/Gutenberg/app/admin.py
+++ Project_Gutenberg/Gutenberg/app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from app.models import Book,Bookshelf,Subject,DownloadLink
 # Register your models here.
-admin.site.register[Book,Bookshelf,Subject,DownloadLink]
+admin.site.register([Book,Bookshelf,Subject,DownloadLink])
